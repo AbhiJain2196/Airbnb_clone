3e4a94d31a44d337abb0363639e5f26499cb0b75
Abhishek <abhijain2196@gmail.com>
2021-11-17 14:01:32 +0530

    Initial Commit

---
+++ components/Map.js
@@ -0,0 +1,68 @@
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useState } from "react";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  console.log(selectedLocation);
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/gejaro/ckw31fnar0sot14pm4locg08i"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetTop={-10}
+            offsetLeft={-20}
+          >
+            <p
+              role="img"
+              aria-label="push-pin"
+              onClick={() => setSelectedLocation(result)}
+              classname="cursor-pointer text-2xl animate-bounce"
+            >
+              📌
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env:{
+    mapbox_key:"pk.eyJ1IjoiZ2VqYXJvIiwiYSI6ImNrdnJ0eXNjYjZyY2czM29rc3R4ZDJlbWEifQ.IT2gcNgotPqEniuPqINHwg"
+  },
+};
